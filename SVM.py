ce8b6bdb30419f5c6279533cdd2a563b78e7f73f
Doris-UESTC <2449116175@qq.com>
2021-04-07 00:35:23 +0800

    4.6 完成支持向量机书写

--- SVM.py
+++ SVM.py
@@ -19,16 +19,25 @@ class SVM:
     def calculateKernel(self):
         k=[[0 for i in range(self.m)] for j in range(self.m) ]
         for i in range(self.m):
-            X=self.trainDataMatrix[i:]
+            X=self.trainDataMatrix[i,:]
             for j in range(i,self.m):
-                Z=self.trainDataMatrix[j:]
+                Z=self.trainDataMatrix[j,:]
                 result=(X-Z)*(X-Z).T
                 result=np.exp((-1)*result/(2*self.sigma**2))
                 k[i][j]=result
                 k[j][i]=result
         return k
 
-    def isSatisfyKKT(self):
+    def isSatisfyKKT(self,i):
+        gxi=self.calculate_gxi(self,i)
+        yi=self.trainLabelMatrix[i]
+        if(math.fabs(self.alpha[i])<self.toler and yi*gxi>=1):
+            return True
+        elif(math.fabs(self.alpha[i]-self.C)<self.toler and yi*gxi<=1):
+            return True
+        elif(self.alpha[i]>-self.toler and self.alpha[i]<self.C+self.toler and math.fabs(gxi*yi-1)<=self.toler):
+            return True
+        return False
 
     def calculate_gxi(self,i):
         gxi=0
@@ -60,4 +69,64 @@ class SVM:
             E2=self.caculate_Ei(maxIndex)
         return E2,maxIndex
 
-    def calculatesingleKernel(self):
+    def calculatesingleKernel(self,x1,x2):
+        result=(x1-x2)*(x1-x2).T
+        result=(-1)*result/(2*self.sigma**2)
+        return np.exp(result)
+    def train(self,iter=100):
+        iterstep=0
+        parameterchanged=1
+        while(iterstep<iter and parameterchanged>0):
+            iter+=1
+            parameterchanged=0
+            for i in range(self.m):
+                if self.isSatisfyKKT(i)==False:
+                    E1=self.caculate_Ei(i)
+                    E2,j=self.getAlphaJ(E1,i)
+                    y1=self.trainLabelMatrix[i]
+                    y2=self.trainLabelMatrix[j]
+                    alpha_1=self.alpha[i]
+                    alpha_2=self.alpha[j]
+                    if(y1!=y2):
+                        L=max(0,alpha_2-alpha_1)
+                        H=min(self.C,self.C+alpha_2-alpha_1)
+                    else:
+                        L=max(0,alpha_2+alpha_1-self.C)
+                        H=min(self.C,alpha_2+alpha_1)
+                    if(L==H):
+                        continue
+                    k11=self.k[i][i]
+                    k12=self.k[i][j]
+                    k21=self.k[j][i]
+                    k22=self.k[j][j]
+                    alpha_new_2=alpha_2+y2*(E1-E2)/(k11+k22-2*k12)
+                    if(alpha_new_2<L):
+                        alpha_new_2=L
+                    elif(alpha_new_2>H):
+                        alpha_new_2=H
+                    alpha_new_1=alpha_1+y1*y2*(alpha_2-alpha_new_2)
+                    b1_new=-1*E1-y1*k11*(alpha_new_1-alpha_1)-y2*k21*(alpha_new_2-alpha_2)+self.b
+                    b2_new=-1*E2-y1*k12*(alpha_new_2-alpha_2)-y2*k22*(alpha_new_2-alpha_2)+self.b
+                    if(alpha_new_1>0 and alpha_new_1<self.C):
+                        b_new=b1_new
+                    elif(alpha_new_2>0 and alpha_new_2<self.C):
+                        b_new=b2_new
+                    else:
+                        b_new=(b1_new+b2_new)/2
+                    self.alpha[i]=alpha_new_1
+                    self.alpha[j]=alpha_new_2
+                    self.b=b_new
+                    self.E[i]=self.caculate_Ei(i)
+                    self.E[j]=self.caculate_Ei(j)
+                    if(math.fabs(alpha_new_2-alpha_2)>=0.00001):
+                        parameterchanged+=1
+        for i in range(self.m):
+            if (self.alpha[i]>0):
+                self.supportVectorIndex.append(i)
+    def predict(self,x):
+        result=0
+        for i in self.supportVectorIndex:
+            tmp=self.calculatesingleKernel(self.trainDataMatrix[i,:],np.matrix(x))
+            result+=self.alpha[i]*self.trainLabelMatrix[i]*tmp
+        result+=self.b
+        return np.sign(result)
